c3a79f92d08b753f923fb491d771792858b23e78
vaso <vasyarovnov0802@gmail.com>
2022-11-05 17:54:56 +0400

    fix modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -5,3 +5,31 @@ document.querySelectorAll('.modal__overlay').forEach(el => {
         modal.querySelector('iframe').remove();
     });
 });
+const link = document.getElementById("link")
+const popupEdit = document.getElementById("popup")
+const closeAdd = document.getElementById("close_popup")
+
+function openPopup (popup) {
+    popup.classList.add ('modal_show');
+    document.addEventListener("keydown", keyHandler);
+  }
+function closePopup (popup) {
+    popup.classList.remove ('modal_show');
+    document.removeEventListener("keydown", keyHandler);
+  }
+function keyHandler(evt) {    
+    if (evt.key === 'Escape') {
+    const popup = document.querySelector('.modal_show');
+    closePopup (popup);
+  }
+  } 
+function editPopup(){    
+    openPopup (popupEdit);
+    editFormValidator.resetValidForm();
+  }
+  function closePopupAdd(evt) {
+    closePopup(popupEdit, evt)
+    /* formAdd.reset() */
+}
+link.addEventListener ('click', editPopup);//добавить
+closeAdd.addEventListener ('click', closePopupAdd);
